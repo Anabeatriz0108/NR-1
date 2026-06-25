499db3a4b7ea4f699c6e4b5f0220d1010a1f7b70
Anabeatriz0108 <bialindonarp@gmail.com>
2026-06-25 16:03:12 -0300

    Fix NR-1 demo text encoding

---
+++ public/demoQuestions.js
@@ -0,0 +1,12 @@
+﻿window.NR1_DEMO_QUESTIONS = [
+    "A empresa possui Programa de Gerenciamento de Riscos (PGR) implementado e atualizado?",
+    "Os treinamentos obrigatórios previstos na NR-1 estão em dia para todos os colaboradores?",
+    "A documentação de saúde e segurança do trabalho está organizada, atualizada e acessível?",
+    "A empresa possui plano de emergência formalizado e comunicado aos trabalhadores?",
+    "Os riscos ocupacionais são identificados, avaliados e acompanhados de forma contínua?",
+    "O fornecimento, uso e controle dos EPIs são registrados e monitorados pela empresa?",
+    "A empresa acompanha requisitos legais e práticas de compliance relacionadas à NR-1?",
+    "São realizadas inspeções periódicas para verificar condições de segurança no ambiente de trabalho?",
+    "A operação possui medidas preventivas para reduzir incidentes, acidentes e desvios de segurança?",
+    "A empresa utiliza indicadores de segurança para acompanhar resultados e definir ações de melhoria?"
+];
